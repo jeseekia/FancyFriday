e3a05bb91e7f7e356da28e5bcae6cf586ef782f0
Jeseekia Vaughn <jeseekia@gmail.com>
2016-09-08 11:23:06 -0400

    Initial commit

---
+++ src/MainApp.java
@@ -0,0 +1,19 @@
+/**
+ * This is a demo app for deciding what to wear.
+ */
+
+/**
+ * @author jeseekia
+ *
+ */
+public class MainApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
